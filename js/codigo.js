f43e34fc7019344f7deec66d2f061368ec545560
pinozu4 <pinozu4@gmail.com>
2019-06-14 15:56:10 -0500

    Proyecto Actualizado

--- js/codigo.js
+++ js/codigo.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
+    
     var contenedores ={ 
         prehisUno: $('.prehisUno').offset().left,
         prehisDos: $('.prehisDos').offset().left,
-        prehisTres: $('.prehisTres').offset().left,
 
         desUno: $('.greciaUno').offset().left,
         desDos: $('.greciaDos').offset().left,
@@ -10,21 +10,25 @@ $(document).ready(function(){
 
         exiUno: $('.romaUno').offset().left,
         exiDos: $('.romaDos').offset().left,
-        exiTres: $('.romaTres').offset().left,
 
         enigUno: $('.enigmaUno').offset().left,
         enigDos: $('.enigmaDos').offset().left,
-        enigTres: $('.enigmaTres').offset().left,
 
         moderUno: $('.modernaUno').offset().left,
         moderDos: $('.modernaDos').offset().left,
         moderTres: $('.modernaTres').offset().left,
+        moderCuatro: $('.modernaCuatro').offset().left,
+        moderCinco: $('.modernaCinco').offset().left,
+        moderSeis: $('.modernaSeis').offset().left,
+        moderSiete: $('.modernaSiete').offset().left,
     }
     //Los edificion tienen una clase llamada 'mamut'
 
     //Inicio
     $('.cartelAvance').css('Animation-play-state', 'running');
 
+
+    $('.pre-carga').fadeOut();
     
 
     //Menu
@@ -35,6 +39,75 @@ $(document).ready(function(){
             scrollLeft: $(atributo).offset().left
         }, 1500, 'easeInOutQuart');
     });
+
+
+    //Botones Pequeños
+    $('.btn-pasar-prehistoria').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#prehistoria-dos').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-aldesnudo').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#aldesnudo-dos').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-aldesnudoTres').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#aldesnudo-tres').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-exilio').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#exilio-dos').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-enigma').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#enigma-dos').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-moderna').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#moderna-dos').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-modernaTres').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#moderna-tres').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-modernaCuatro').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#moderna-cuatro').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-modernaCinco').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#moderna-cinco').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-modernaSeis').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#moderna-seis').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
+    $('.btn-pasar-modernaSiete').on('click', function(){
+        $('body, html').stop().animate({
+            scrollLeft: $('#moderna-siete').offset().left+5
+        }, 1500, 'easeInOutQuart');
+    });
+
 
     //Icono Sonido
     $('.iconoSonido li').mouseenter(function(){
@@ -56,9 +129,11 @@ $(document).ready(function(){
             height: '15px',
         }, 500, 'easeInBack');
     })
-
-    
-
+    /* $('.iconosSecciones li').mouseenter(function(){
+        
+        $(this).fadeIn()
+    }) */
+    $('.cero').addClass('activo2')
     //Fin de Inicio
 
 
@@ -71,10 +146,12 @@ $(document).ready(function(){
     var arbolesPrehistoria = {
         Uno: $('.prehisUno .moverArboles'),
         Dos: $('.prehisDos .moverArboles'),
-        Tres: $('.prehisTres .moverArboles')
     };
     $(window).on('scroll', function(){
-        if($(window).scrollLeft() >= contenedores.prehisUno){
+        if($(window).scrollLeft() >= contenedores.prehisUno-50){
+            $('.btn-pasar-prehistoria').fadeIn();
+            $('.activo').removeClass('activo2');
+            $('.uno').addClass('activo2')
             $('.nubesEvento').css('position','fixed');
             $('.nubesEvento').css('z-index','1');
             nubesPrehistoria.impares.animate({
@@ -103,6 +180,8 @@ $(document).ready(function(){
         }else{
             $('.nubesEvento').css('position','absolute');
             sol.css('position','absolute');
+            $('.activo').removeClass('activo2');
+            $('.cero').addClass('activo2')
         }
 
         if($(window).scrollLeft() >= contenedores.prehisDos){
@@ -112,26 +191,58 @@ $(document).ready(function(){
             $('.cartelPrehi').animate({
                 bottom: '250px',
             }, 1200, 'easeOutElastic');
-        }
-        if($(window).scrollLeft() >= contenedores.prehisTres){
-            arbolesPrehistoria.Tres.animate({
-                top: '0',
+            $('.cavernicola1').animate({
+                bottom: '10px',
             }, 1000, 'easeOutBounce');
-            $('.cavernicola').animate({
-                top: '-100px',
-            }, 1000, 'easeOutBounce');
-            $('.madero').animate({
-                top: '-200px',
-            }, 1300, 'easeOutBounce');
-            $('.animacionPre').animate({
-                top: '210px',
-            }, 1500, 'easeOutBounce');
-            $('.pAnimacion').animate({
-                right: '0',
-                opacity: '1',
-            }, 1500, 'easeOutBounce');
         }
     });
+    /* ================ Infografía Origen ============== */
+    $('.infoOri1').hide()
+    
+    $('.iconoMensaje').click(function(){
+        $('.infoOri1').slideToggle(1000);
+
+    })
+    $('.infoOri1 .btnBack').click(function(){
+        $('.infoOri1').hide(1000);
+    })
+
+    $('.infoOri1 .btnOri').click(function(){
+        $('.contInfo').slideToggle();
+    })
+    $('.infoOri2 .btnOri2').click(function(){
+        $('.infoOri2 .contInfo').slideToggle();
+    })
+    $('.infoOri3 .btnOri2').click(function(){
+        $('.infoOri3 .contInfo').slideToggle();
+    })
+    $('.infoOri2').hide();
+    $('.infoOri1 .btnNextOri').click(function(){
+        $('.infoOri1').hide(1500);
+        $('.infoOri2').show(500);
+    })
+    $('.infoOri2 .btnBack').click(function(){
+        $('.infoOri2').hide(1500);
+        $('.infoOri1').show(500);
+    })
+    $('.infoOri3').hide();
+    $('.infoOri2 .btnNextOri').click(function(){
+        $('.infoOri2').hide(1500);
+        $('.infoOri3').show(500);
+    })
+    $('.infoOri3 .btnBack').click(function(){
+        $('.infoOri3').hide(1500);
+        $('.infoOri2').show(500);
+    })
+    $('.infoOri3 .btnNextOri').click(function(){
+        $('.infoOri3').slideToggle(1000);
+    })
+
+
+
+    
+
+    /* ================ Fin de Infografía Origen ============== */
     //Fin de Origen
 
 
@@ -143,7 +254,11 @@ $(document).ready(function(){
     };
 
     $(window).on('scroll', function(){
-        if($(window).scrollLeft() >= contenedores.desUno){
+        if($(window).scrollLeft() >= contenedores.desUno-50){
+            $('.btn-pasar-aldesnudo').fadeIn();
+            $('.btn-pasar-aldesnudoTres').fadeIn();
+            $('.activo').removeClass('activo2');
+            $('.dos').addClass('activo2');
             arbolesDesnudo.Uno.animate({
                 top: '0',
             }, 1000, 'easeOutBounce');
@@ -163,13 +278,13 @@ $(document).ready(function(){
                 left: "0px",
                 opacity: "1",
             }, 1500, 'easeOutBounce');
-            $('.greciaDos .introduccion p').animate({
+            $('.alDesnudo .greciaDos .introduccion p').animate({
                 left: "0px",
                 opacity: "1",
                 }, 1500, 'easeOutBounce');
-                $('.edificioGrecia').animate({
-                    top: "0px",
-                }, 2000, 'easeOutBounce');
+            $('.edificioGrecia').animate({
+                top: "0px",
+            }, 2000, 'easeOutBounce');
                 
            
         }
@@ -185,10 +300,10 @@ $(document).ready(function(){
                 top: "100px",
             }, 2000, 'easeOutBounce');
             $('.griego').animate({
-                top: "-150px",
+                bottom: "10px",
             }, 2000, 'easeOutBounce');
-            $('.alDesnudo .animacion p').animate({
-                opacity: "1",
+            $('.alDesnudo .animacion video').animate({
+                opacity: '1',
                 }, 1500, 'easeOutBounce');
            
         }
@@ -203,11 +318,13 @@ $(document).ready(function(){
     var arbolesExilio = {
         Uno: $('.romaUno .moverArboles'),
         Dos: $('.romaDos .moverArboles'),
-        Tres: $('.romaTres .moverArboles'),
     };
 
     $(window).on('scroll', function(){
-        if($(window).scrollLeft() >= contenedores.exiUno){
+        if($(window).scrollLeft() >= contenedores.exiUno-50){
+            $('.btn-pasar-exilio').fadeIn();
+            $('.activo').removeClass('activo2');
+            $('.tres').addClass('activo2');
             arbolesExilio.Uno.animate({
                 top: '0',
             }, 1000, 'easeOutBounce');
@@ -228,27 +345,62 @@ $(document).ready(function(){
             $('.romaDos .introduccion p').animate({
                 opacity: "1",
             }, 4000, 'easeOutBounce');
-        }
-        if($(window).scrollLeft() >= contenedores.exiTres){
-            arbolesExilio.Tres.animate({
-                top: '0',
-            }, 1000, 'easeOutBounce');
             $('.romano').animate({
-                top: '-150px'
+                bottom: '15px'
             }, 1000, 'easeOutBounce');
-            $('.roma .animacion p').animate({
-                top: '300px'
-            }, 1000, 'easeOutBounce');
-            $('.roma .animacion img:nth-child(2)').animate({
-                opacity: '1'
-            }, 1000, 'easeOutBounce');
-            $('.roma .animacion img:nth-child(1)').css('Animation-play-state', 'running');
-    
-            
         }
+        
         
 
     })
+
+
+    /* ================ Infografía Roma ============== */
+    $('.infoRoma1').hide()
+    $('.iconoMensajeRoma').click(function(){
+        $('.infoRoma1').slideToggle(1000);
+
+    })
+    $('.infoRoma1 .btnBack').click(function(){
+        $('.infoRoma1').hide(1000);
+    })
+
+    $('.infoRoma1 .btnRoma').click(function(){
+        $('.contInfo').slideToggle();
+    })
+    $('.infoRoma2 .btnRoma2').click(function(){
+        $('.infoRoma2 .contInfo').slideToggle();
+    })
+    $('.infoRoma3 .btnRoma2').click(function(){
+        $('.infoRoma3 .contInfo').slideToggle();
+    })
+    $('.infoRoma2').hide();
+    $('.infoRoma1 .btnNext').click(function(){
+        $('.infoRoma1').hide(1500);
+        $('.infoRoma2').show(500);
+    })
+    $('.infoRoma2 .btnBack').click(function(){
+        $('.infoRoma2').hide(1500);
+        $('.infoRoma1').show(500);
+    })
+    $('.infoRoma3').hide();
+    $('.infoRoma2 .btnNext').click(function(){
+        $('.infoRoma2').hide(1500);
+        $('.infoRoma3').show(500);
+    })
+    $('.infoRoma3 .btnBack').click(function(){
+        $('.infoRoma3').hide(1500);
+        $('.infoRoma2').show(500);
+    })
+    $('.infoRoma3 .btnNext').click(function(){
+        $('.infoRoma3').slideToggle(1000);
+    })
+
+
+
+    
+
+    /* ================ Fin de Infografía Roma ============== */
     //Fin de Roma
 
 
@@ -256,10 +408,12 @@ $(document).ready(function(){
     var arbolesEnigma = {
         Uno: $('.enigmaUno .moverArboles'),
         Dos: $('.enigmaDos .moverArboles'),
-        Tres: $('.enigmaTres .moverArboles'),
     };
     $(window).on('scroll', function(){
-        if($(window).scrollLeft() >= contenedores.enigUno){
+        if($(window).scrollLeft() >= contenedores.enigUno-50){
+            $('.btn-pasar-enigma').fadeIn();
+            $('.activo').removeClass('activo2');
+            $('.cuatro').addClass('activo2');
             arbolesEnigma.Uno.animate({
                 top: '0',
             }, 1000, 'easeOutBounce');
@@ -282,30 +436,55 @@ $(document).ready(function(){
             $('.enigmaDos .introduccion p').animate({
                 top:"0px",
             }, 1000, 'easeOutBounce');
+            $('.arlequin').animate({
+                bottom: "25px",
+                opacity:"1",
+                }, 1500, 'easeOutBounce');
         }
-
-        $(window).on('scroll', function(){
-            if($(window).scrollLeft() >= contenedores.enigTres){
-                arbolesEnigma.Tres.animate({
-                    top: '0',
-                }, 1000, 'easeOutBounce');
-                $('.arlequin').animate({
-                    top: "-160px",
-                    opacity:"1",
-                    }, 1500, 'easeOutBounce');
-                $('.enigmaTres .animacion img:nth-child(2)').animate({
-                    top: "270px",
-                }, 2000, 'easeOutBounce');
-                $('.enigmaTres .animacion img:nth-child(1)').animate({
-                    top: "-150px",
-                    }, 1500, 'easeOutBounce');
-                $('.enigmaTres .animacion p').animate({
-                    top:"500px",
-                }, 1000, 'easeOutBounce');
-                    
-            }
-        })
     })
+    /* ================ Infografía Enigma ============== */
+    $('.infoEnig1').hide()
+    $('.iconoMensajeEnigma').click(function(){
+        $('.infoEnig1').slideToggle(1000);
+
+    })
+    $('.infoEnig1 .btnBack').click(function(){
+        $('.infoEnig1').hide(1000);
+    })
+
+    $('.infoEnig1 .btnEnig').click(function(){
+        $('.contInfo').slideToggle();
+    })
+    $('.infoEnig2 .btnEnig2').click(function(){
+        $('.infoEnig2 .contInfo').slideToggle();
+    })
+    $('.infoEnig2').hide();
+    $('.infoEnig1 .btnNext').click(function(){
+        $('.infoEnig1').hide(1500);
+        $('.infoEnig2').show(500);
+    })
+    $('.infoEnig2 .btnBack').click(function(){
+        $('.infoEnig2').hide(1500);
+        $('.infoEnig1').show(500);
+    })
+    $('.infoEnig3').hide();
+   /*  $('.infoEnig2 .btnNext').click(function(){
+        $('.infoEnig2').hide(1500);
+        $('.infoEnig3').show(500);
+    }) */
+    $('.infoEnig3 .btnBack').click(function(){
+        $('.infoEnig3').hide(1500);
+        $('.infoEnig2').show(500);
+    })
+    $('.infoEnig2 .btnNext').click(function(){
+        $('.infoEnig2').slideToggle(1000);
+    })
+
+
+
+    
+
+    /* ================ Fin de Infografía Enigma ============== */
 
 
     //Fin deEnigma
@@ -316,10 +495,22 @@ $(document).ready(function(){
         Uno: $('.modernaUno .moverArboles'),
         Dos: $('.modernaDos .moverArboles'),
         Tres: $('.modernaTres .moverArboles'),
+        Cuatro: $('.modernaCuatro .moverArboles'),
+        Cinco: $('.modernaCinco .moverArboles'),
+        Seis: $('.modernaSeis .moverArboles'),
+        Siete: $('.modernaSiete .moverArboles'),
     };
 
     $(window).on('scroll', function(){
-        if($(window).scrollLeft() >= contenedores.moderUno){
+        if($(window).scrollLeft() >= contenedores.moderUno-50){
+            $('.btn-pasar-moderna').fadeIn();
+            $('.btn-pasar-modernaTres').fadeIn();
+            $('.btn-pasar-modernaCuatro').fadeIn();
+            $('.btn-pasar-modernaCinco').fadeIn();
+            $('.btn-pasar-modernaSeis').fadeIn();
+            $('.btn-pasar-modernaSiete').fadeIn();
+            $('.activo').removeClass('activo2');
+            $('.cinco').addClass('activo2');
             arbolesModerna.Uno.animate({
                 top: '0',
             }, 1000, 'easeOutBounce');
@@ -339,8 +530,8 @@ $(document).ready(function(){
             $('.modernaDos .introduccion p').animate({
                 opacity: '1' 
             }, 3000, 'easeOutBounce').fadeIn();
-            $('.introduccion').animate({
-                bottom: '300px' 
+            $('.modernaDos .introduccion img').animate({
+                top: '-50px' 
             }, 1500, 'easeOutBounce').fadeIn();
             $('.modernaDos .coliseoGim').animate({
                 top: '0px' 
@@ -351,21 +542,77 @@ $(document).ready(function(){
             arbolesModerna.Tres.animate({
                 top: '0',
             }, 1000, 'easeOutBounce');
+            
+        }
+        if($(window).scrollLeft() >= contenedores.moderCuatro){
+            arbolesModerna.Cuatro.animate({
+                top: '0',
+            }, 1000, 'easeOutBounce');
             $('.nadia').animate({ 
-                top: '-50px' 
+                bottom: '10px' 
             }, 1000, 'easeOutBounce').fadeIn();
-            $('.moderna .animacion img:nth-child(1)').animate({
-                top: '80px' 
+            $('.moderna .animacion audio').animate({
+                top: '300px' 
             }, 1700, 'easeOutBounce').fadeIn();
-            $('.moderna .animacion img:nth-child(2)').animate({
-                top: '160px' 
-            }, 2000, 'easeOutBounce').fadeIn();
             $('.moderna .animacion p').animate({
                 top: '400px', 
                 opacity: '1' 
             }, 3400, 'easeInOutQuart').fadeIn();
         }
+        if($(window).scrollLeft() >= contenedores.moderCinco){
+            arbolesModerna.Cinco.animate({
+                top: '0',
+            }, 1000, 'easeOutBounce');
+            $('.modernaCinco .borbuja img').animate({
+                bottom: '140px',
+            }, 1400, 'easeOutBounce');
+            $('.modernaCinco .borbuja p').css('opacity', '1');
+        }
+        if($(window).scrollLeft() >= contenedores.moderSeis){
+            arbolesModerna.Seis.animate({
+                top: '0',
+            }, 1000, 'easeOutBounce');
+            $('.modernaSeis .anillos img').animate({
+                top: '0',
+            }, 1400, 'easeOutBounce');
+            $('.modernaSeis .anillos p').css('opacity', '1');
+        }
+        if($(window).scrollLeft() >= contenedores.moderSiete){
+            arbolesModerna.Siete.animate({
+                top: '0',
+            }, 1000, 'easeOutBounce');
+            $('.modernaSiete .tv img').css('opacity', '1');
+            $('.modernaSiete .tv p').animate({
+                top: '250px',
+                opacity: '1'
+            }, 1700, 'easeOutBounce').fadeIn();
+        }
     })        
     //Fin de Moderna
+
+
+    /* ================ Ventana Realidad Aumentada ============== */
+
+    $('.ventana_RA').hide();
+    $('.icon_RA').click(function(){
+        $('.ventana_RA').slideToggle(1000);
+    });
+
+    $('.btn_cerrar').click(function(){
+        $('.ventana_RA').hide(1000);
+    });
+
+    $('.banderas_RA div img').on('click', function(){
+        var img = $(this).attr('ruta');
+        $('#fondo .imagen img').attr('src', img);
+        $('#fondo').fadeIn();
+    });
+
+    $('#fondo').on('click', function(){
+        $('#fondo').fadeOut();
+    });
+
+
+    /* ================ Fin Ventana Realidad Aumentada ============== */
 
 })
